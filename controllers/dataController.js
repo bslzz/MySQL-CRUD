4387115264c21225b4d7640d29948eadb5960dcf
bslzz <bishalkoid@gmail.com>
2020-11-14 20:09:45 +0200

    created get request to show data from db to frontend

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,20 +1,49 @@
 const db = require('../db');
 
 module.exports = {
+  //post request
   postData: async (req, res) => {
     try {
       const { name, age, country, position, wage } = req.body;
-      db.query(
+      await db.query(
         'INSERT into employee(name, age, country, position, wage) VALUES(?,?,?,?,?)',
         [name, age, country, position, wage],
         (err, result) => {
           if (err) {
             console.log(err);
           } else {
-            console.log(result);
+            res.status(200).json({
+              success: true,
+              data: result,
+            });
           }
         }
       );
-    } catch (error) {}
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        msg: error.message,
+      });
+    }
+  },
+
+  getData: async (req, res) => {
+    try {
+      await db.query('SELECT * from employee', (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).json({
+            success: true,
+            data: result,
+          });
+        }
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        msg: error.message,
+      });
+    }
   },
 };
--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const dataController = require('../controllers/dataController');
 
 router.route('/post').post(dataController.postData);
+router.route('/get').get(dataController.getData);
 
 module.exports = router;
