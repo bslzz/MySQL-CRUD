7c445ab8035907f6823a3d0da4c2cdd2fb64ea5c
bslzz <bishalkoid@gmail.com>
2020-11-14 19:25:18 +0200

    created controller folder to create requests

---
+++ controllers/dataController.js
@@ -0,0 +1,20 @@
+const db = require('../db');
+
+module.exports = {
+  postData: async (req, res) => {
+    try {
+      const { name, age, country, position, wage } = req.body;
+      db.query(
+        'INSERT into employees(name, age, country, position, wage) VALUES(?,?,?,?,?)',
+        [name, age, country, position, wage],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(result);
+          }
+        }
+      );
+    } catch (error) {}
+  },
+};
