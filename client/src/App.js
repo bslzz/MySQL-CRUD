90c88fed3f5c2a53b2026d17e2700a5dad3a6392
bslzz <bishalkoid@gmail.com>
2020-11-14 19:06:22 +0200

    created form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,46 @@
 import React from 'react';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <form class="row g-3">
+      <div class="col-md-6">
+        <label for="name" class="form-label">
+          Name
+        </label>
+        <input type="text" class="form-control" id="name" />
+      </div>
+      <div class="col-md-6">
+        <label for="age" class="form-label">
+          Age
+        </label>
+        <input type="number" class="form-control" id="age" />
+      </div>
+      <div class="col-md-6">
+        <label for="country" class="form-label">
+          Country
+        </label>
+        <input type="text" class="form-control" id="country" />
+      </div>
+      <div class="col-md-6">
+        <label for="position" class="form-label">
+          Position
+        </label>
+        <input type="text" class="form-control" id="position" />
+      </div>
+      <div class="col-md-6">
+        <label for="wage" class="form-label">
+          Wage
+        </label>
+        <input type="number" class="form-control" id="wage" />
+      </div>
+
+      <div class="col-12">
+        <button type="submit" class="btn btn-primary">
+          Save
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default App;
